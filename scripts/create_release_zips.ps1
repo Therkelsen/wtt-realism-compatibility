e17f268d9dc6de95df822bce1627314a0a12c8e3
Therkelsen <37107686+Therkelsen@users.noreply.github.com>
2024-09-30 20:13:42 +0200

    Add Barret M107A1 compats

---
+++ scripts/create_release_zips.ps1
@@ -0,0 +1,37 @@
+param (
+    [string]$mod_version,
+    [string]$spt_version
+)
+
+# Define the release directory
+$releaseDir = ".\release"
+
+# Create release directory if it doesn't exist
+if (-Not (Test-Path -Path $releaseDir)) {
+    New-Item -ItemType Directory -Path $releaseDir -Force
+}
+
+# Create directory names within the release directory
+$baseDir = "$releaseDir\wtt-realism-compatibility_${mod_version}_SPT-v${spt_version}"
+
+# Define the inner directory structure
+$innerDir = "user\mods\SPT-Realism\db\put_new_stuff_here\WTT Weapons"
+
+# Create the directories
+New-Item -ItemType Directory -Path "$baseDir\$innerDir" -Force
+
+# Define the list of files to move
+$filesToMove = @(
+    'WTT Weapons\Barrett_M107A1.json'
+)
+
+# Move files to both directories
+foreach ($file in $filesToMove) {
+    Copy-Item -Path $file -Destination "$baseDir\$innerDir"
+}
+
+# Create zip files
+Compress-Archive -Path "$baseDir\*" -DestinationPath "$releaseDir\wtt-realism-compatibility_${mod_version}_SPT-v${spt_version}.zip" -Force
+
+# Clean up the created directories
+Remove-Item -Recurse -Force "$baseDir"
