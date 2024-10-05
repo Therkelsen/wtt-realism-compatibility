ab7e00f9af3fbac5c0c2f48ca6d4cedb3bc353ff
Therkelsen <37107686+Therkelsen@users.noreply.github.com>
2024-10-05 09:03:26 +0200

    Added support for Rexana's ACR

--- scripts/create_release_zips.ps1
+++ scripts/create_release_zips.ps1
@@ -27,6 +27,7 @@ $filesToMove = @(
     'WTT Weapons\Beretta_ARX160.json',
     'WTT Weapons\Desert_Eagle.json',
     'WTT Weapons\Hoshizora_SCAR_17.json',
+    'WTT Weapons\Remington_ACR.json',
     'WTT Weapons\Remington_R5.json'
     'WTT Weapons\Zastava_M76.json'
 )
