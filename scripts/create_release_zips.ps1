3489919170f0bd56c759f8fe89e08e341efa4475
Therkelsen <37107686+Therkelsen@users.noreply.github.com>
2024-10-10 21:23:41 +0200

    Added support for ERT's M700 Long Action

--- scripts/create_release_zips.ps1
+++ scripts/create_release_zips.ps1
@@ -38,6 +38,7 @@ $filesToMove = @(
     'WTT Armory\IWI_Tavor_TAR21.json',
     'WTT Armory\IWI_Tavor_X95.json',
     'WTT Armory\M249_SAW.json',
+    'WTT Armory\M700LA.json',
     'WTT Armory\Patriot_1776.json',
     'WTT Armory\Remington_ACR.json',
     'WTT Armory\Remington_MSR.json',
